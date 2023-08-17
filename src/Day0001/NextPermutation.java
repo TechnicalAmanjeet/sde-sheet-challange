b38b93e0279ec51bad80a216dfdee28e0d25862c
Amanjeet Kumar <amanjeet.kumar@cogoport.com>
2023-08-17 19:27:42 +0530

    done with day001 nextPermutation

--- src/Day0001/NextPermutation.java
+++ src/Day0001/NextPermutation.java
@@ -4,24 +4,44 @@ public class NextPermutation {
     public static void main(String[] args) {
         Solution3 obj = new Solution3();
 
-        int[] array = {1,2,3,4,5,6};
-        for (int number: obj.revereArray(array, 1, 2)) {
+        int[] array = {1,3,2};
+        for (int number: obj.nextPermutation(array)) {
             System.out.printf(" " + number);
         }
         System.out.println();
     }
+
 }
 
 class Solution3 {
-    public void nextPermutation(int[] nums) {
+    public int[] nextPermutation(int[] nums) {
 
-        for (int i=nums.length-1; i>=0; ++i) {
+        for (int i=nums.length-1; i>=0; --i) {
             if (i == 0) {
-                nums = revereArray(nums, i, nums.length-1);
+                nums = revereArrayUsingWhile(nums, i, nums.length-1);
+                break;
+            }
+
+            if (nums[i-1] < nums[i]) {
+                int swapIndex = nums.length - 1;
+                for (int k = nums.length - 1; k>=i; --k) {
+                    if (nums[k] > nums[i-1]) {
+                        swapIndex = k;
+                        break;
+                    }
+                }
+
+                nums[i-1] = nums[i-1] ^ nums[swapIndex];
+                nums[swapIndex] = nums[i-1] ^ nums[swapIndex];
+                nums[i-1] = nums[i-1] ^ nums[swapIndex];
+
+                revereArrayUsingWhile(nums, i, nums.length-1);
                 break;
             }
 
         }
+
+        return nums;
     }
 
     public int[] revereArray(int[] nums, int startIndex, int lastIndex) {
@@ -35,5 +55,19 @@ class Solution3 {
 
         return nums;
     }
+
+    public int[] revereArrayUsingWhile(int[] nums, int startIndex, int lastIndex) {
+        while (startIndex < lastIndex) {
+            nums[startIndex] = nums[startIndex] ^ nums[lastIndex];
+            nums[lastIndex] = nums[startIndex] ^ nums[lastIndex];
+            nums[startIndex] = nums[startIndex] ^ nums[lastIndex];
+
+            ++startIndex;
+            --lastIndex;
+        }
+
+        return nums;
+    }
+
 
 }
